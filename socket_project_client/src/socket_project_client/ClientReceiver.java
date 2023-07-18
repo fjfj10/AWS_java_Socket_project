1775f66c0e7dab6953c4a68768159df47a3f1d78
younghk37 <skvssnsk@daum.net>
2023-07-18 09:20:31 +0900

    refactor: ClientReceiver 클래스의 switch 문 -> 메소드로 분리

--- socket_project_client/src/socket_project_client/ClientReceiver.java
+++ socket_project_client/src/socket_project_client/ClientReceiver.java
@@ -18,10 +18,12 @@ import socket_project_client.dto.RequestBodyDto;
 
 public class ClientReceiver extends Thread{
 	
-	
+	private Gson gson;
 	
 	@Override
 	public void run() {
+		gson = new Gson();
+		
 		ProjectClient projectClient = ProjectClient.getInstance();
 		while (true) {
 			try {
@@ -39,55 +41,72 @@ public class ClientReceiver extends Thread{
 		}
 		
 	}
+	
 	private void requestController(String requestBody) {
-		Gson gson = new Gson();
-		
 		String resorce = gson.fromJson(requestBody, RequestBodyDto.class).getResource();
 		switch (resorce) {
 			case "checkUserName":
-				boolean isUsernameDuplicated = (boolean) gson.fromJson(requestBody, RequestBodyDto.class).getBody();
-				if(isUsernameDuplicated == true) {
-					JOptionPane.showMessageDialog(null, "중복된 ID입니다.", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);
-					System.exit(0);
-				}else {
-					JOptionPane.showMessageDialog(null, "사용 가능한 ID입니다.", "", JOptionPane.PLAIN_MESSAGE);
-					
-				}
+				checkUserName(requestBody);
 				break;
 				
 			case "updateRoomList":
-				String searchCondition = ProjectClient.getInstance().getSearchRoomTextField().getText();
-				List<String> roomList = (List<String>) gson.fromJson(requestBody, RequestBodyDto.class).getBody();
-				
-				if(!searchCondition.equals("")) {
-					roomList = roomList.stream().filter((room) -> {
-						return room.contains(searchCondition);
-					}).collect(Collectors.toList());					
-				}
-				
-				ProjectClient.getInstance().getRoomListModel().clear();
-				ProjectClient.getInstance().getRoomListModel().addAll(roomList);
+				updateRoomList(requestBody);
 				break;
 		
 			case "showMessage":
-				String messageContent = (String) gson.fromJson(requestBody, RequestBodyDto.class).getBody();
-				ProjectClient.getInstance().getChattingTextArea().append(messageContent + "\n"); 
+				showMessage(requestBody);
 				break;
 			
 			case "updateUserList":
-				List<String> usernameList = (List<String>) gson.fromJson(requestBody, RequestBodyDto.class).getBody();
-				DefaultListModel<String> userListModel = ProjectClient.getInstance().getUserListModel();
-				userListModel.clear();
-				userListModel.addAll(usernameList);
-				userListModel.set(0, userListModel.get(0) + "(방장)");
+				updateUserList(requestBody);
 				break;
 			
 			case "leave":
-				ProjectClient projectClient = ProjectClient.getInstance();
-				projectClient.getMainCardLayout().show(projectClient.getMainCardPanel(), "chattingRoomListPanel");
-				ProjectClient.getInstance().getChattingTextArea().setText("");
+				leave(requestBody);
 		}
 	}
 	
+	private void checkUserName(String requestBody) {
+		boolean isUsernameDuplicated = (boolean) gson.fromJson(requestBody, RequestBodyDto.class).getBody();
+		if(isUsernameDuplicated == true) {
+			JOptionPane.showMessageDialog(null, "중복된 ID입니다.", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}else {
+			JOptionPane.showMessageDialog(null, "사용 가능한 ID입니다.", "", JOptionPane.PLAIN_MESSAGE);
+			
+		}
+	}
 	
+	private void updateRoomList(String requestBody) {
+		String searchCondition = ProjectClient.getInstance().getSearchRoomTextField().getText();
+		List<String> roomList = (List<String>) gson.fromJson(requestBody, RequestBodyDto.class).getBody();
+		
+		if(!searchCondition.equals("")) {
+			roomList = roomList.stream().filter((room) -> {
+				return room.contains(searchCondition);
+			}).collect(Collectors.toList());					
+		}
+		
+		ProjectClient.getInstance().getRoomListModel().clear();
+		ProjectClient.getInstance().getRoomListModel().addAll(roomList);
+	}
+	
+	private void showMessage(String requestBody) {
+		String messageContent = (String) gson.fromJson(requestBody, RequestBodyDto.class).getBody();
+		ProjectClient.getInstance().getChattingTextArea().append(messageContent + "\n");
+	}
+	
+	private void updateUserList(String requestBody) {
+		List<String> usernameList = (List<String>) gson.fromJson(requestBody, RequestBodyDto.class).getBody();
+		DefaultListModel<String> userListModel = ProjectClient.getInstance().getUserListModel();
+		userListModel.clear();
+		userListModel.addAll(usernameList);
+		userListModel.set(0, userListModel.get(0) + "(방장)");
+	}
+	
+	private void leave(String requestBody) {
+		ProjectClient projectClient = ProjectClient.getInstance();
+		projectClient.getMainCardLayout().show(projectClient.getMainCardPanel(), "chattingRoomListPanel");
+		ProjectClient.getInstance().getChattingTextArea().setText("");
+	}
 }
